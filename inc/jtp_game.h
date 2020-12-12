fe87b8c56b6891f83645a17059221810c374e3b7
Justin Hamilton <justinanthonyhamilton@gmail.com>
2020-12-12 00:19:33 -0800

    Adding Size, defining Clear

--- inc/jtp_game.h
+++ inc/jtp_game.h
@@ -1,13 +1,17 @@
 #ifndef JTP_GAME_H
 #define JTP_GAME_H
+#include "jtp_math.h"
 #include "jtp_types.h"
 #include <stdbool.h>
 
-void WindowInit(uint width, uint height, const char* title);
+void WindowInit(uint width, uint height, const char *title);
+void WindowClear(Color color);
 void WindowClose(void);
 bool WindowShouldClose(void);
 void ProcessInput(void);
 void BeginDrawing(void);
 void EndDrawing(void);
 float GetTime(void);
+Size WindowSize(void);
+
 #endif
