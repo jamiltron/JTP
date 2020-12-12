b6f4d4e2d63cdc02071fe826495a7472b8f49754
Justin Hamilton <justinanthonyhamilton@gmail.com>
2020-12-12 13:37:23 -0800

    Testing out triangle

--- res/shaders/default.vert
+++ res/shaders/default.vert
@@ -1,10 +1,10 @@
 #version 330 core
 layout (location = 0) in vec3 pos;
-layout (location = 1) in vec3 color;
+// layout (location = 1) in vec3 color;
 
 out vec4 Color;
 
 void main() {
   gl_Position = vec4(pos, 1.0);
-  Color = vec4(color, 1.0);
+  Color = vec4(0.0, 1.0, 0.0, 1.0);
 }
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "jtp_game.h"
+#include "jtp_math.h"
+#include "jtp_shapes2d.h"
 #include "jtp_types.h"
 
 int main() {
@@ -11,6 +13,7 @@ int main() {
 
     BeginDrawing();
     WindowClear((Color) { .r = 0.91f, .g = 0.45f, .b = 0.32f, .a = 1.0f });
+    DrawTriangle((Vec2) { .x = 0, .y = 0}, (Vec2) { .x = 0, .y = 0}, (Vec2) { .x = 0, .y = 0}, (Color) { .r = 0.0f, .g = 1.0f, .b = 0.0f, .a = 1.0f});
     EndDrawing();
   }
 
