573afba9b3b7b4be0cdd3b621d6602dbad2943b8
Justin Hamilton <justinanthonyhamilton@gmail.com>
2020-12-12 00:21:27 -0800

    Basic shader loading

--- inc/jtp_assets.h
+++ inc/jtp_assets.h
@@ -1,6 +1,8 @@
 #ifndef JTP_ASSETS_H
 #define JTP_ASSETS_H
+#include "jtp_shader_program.h"
 
-const char* ReadFile(const char* path);
+const char *ReadFile(const char* path);
+ShaderProgram* LoadShader(const char* vertPath, const char* fragPath);
 
 #endif
