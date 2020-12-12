977ede04342f38ffba83b3e811ca85493a566560
jamiltron <justinanthonyhamilton@gmail.com>
2020-12-11 19:25:46 -0800

    Basic math types

---
+++ inc/jtp_math.h
@@ -0,0 +1,20 @@
+#ifndef JTP_MATH_H
+#define JTP_MATH_H
+
+typedef struct Vec2 {
+  float x, y;
+};
+
+typedef struct Vec3 {
+  float x, y, z;
+};
+
+typedef struct Rect {
+  float x, y, width, height; 
+} Rect;
+
+typedef struct Size {
+  float width, height;
+} Size;
+
+#endif
